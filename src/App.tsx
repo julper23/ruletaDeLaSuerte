2afb464168965eaf6bbf54704db813066acc5428
jperezcoca <jperezcoca@ceit.es>
2023-07-23 14:50:59 +0200

    añadida opcion de cambiar colores en participantes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,57 @@
 import { useState,useEffect } from 'react'
 import './App.css'
+import { Space, Table, Tag } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import { HexColorPicker } from "react-colorful";
 
 function App() {
-  /*const array_concursantes = [
+
+  const [array_concursantes, setArray_concursantes] = useState([
+    {
+      key:1,
+      nombre:"1",
+      color:"#00FF00",
+    },
+    {
+      key:2,
+      nombre:"2",
+      color:"#0000FF",
+    }
+  ]);
+
+  const changeColor = (color,item) => {
+
+    let cambios = [...arrayCambiosCon]
+    item.color = color
+
+    setArray_concursantes(cambios.map((i)=>{
+      if(item.key===i.key){return item}
+      return i
+    }))
+    
+  }
+
+  interface DataType {
+    key:number;
+    nombre: string;
+    color: any;
+  }
+
+  const columns : ColumnsType<DataType> = [
+    {
+      title:"Nombre",
+      dataIndex:"nombre",
+      render: (text)=><p>{text}</p>
+    },
+    {
+      title:"Color",
+      render: (item)=><section className="small"><HexColorPicker color={item.color} onChange={(color)=>{changeColor(color,item)}} /></section>
+    }
+  ]
+
+
+
+  /*
     'Julen',
     'Alex',
     'Txipi',
@@ -12,13 +61,8 @@ function App() {
     'Jongar',
     'Sanchito',
     'Marcos',
-  ];*/
-  const array_concursantes = [
-    'Julen',
-    'txipi'
-  ];
+  */
 
-  const [rotation, setRotation] = useState(0);
   const [isSpinning, setIsSpinning] = useState(false);
   const [winner, setWinner] = useState(null);
 
@@ -46,20 +90,25 @@ function App() {
       context.moveTo(center, center);
       context.arc(center, center, center - 20, i * 2 * Math.PI / array_concursantes.length, (i + 1) * 2 * Math.PI / array_concursantes.length);
       context.lineTo(center, center);
-      context.fillStyle = random_color();
+      context.fillStyle = array_concursantes[i].color;
       context.fill();
 
       context.save();
       context.translate(center, center);
       context.rotate(3 * 2 * Math.PI / (5 * array_concursantes.length) + i * 2 * Math.PI / array_concursantes.length);
       context.translate(-center, -center);
-      context.font = "13px Comic Sans MS";
+      context.font = "27px Comic Sans MS";
       context.textAlign = "right";
       context.fillStyle = "white";
-      context.fillText(array_concursantes[i], canvas.width - 30, center);
+      context.fillText(array_concursantes[i].nombre, canvas.width - 35, center);
       context.restore();
     }
-  }, []);
+  }, [array_concursantes]);
+
+  const cambiarConcursantes = () => {
+    setArray_concursantes(arrayCambiosCon)
+  }
+
 
   function random_color() {
     let ar_digit = ['2', '3', '4', '5', '6', '7', '8', '9'];
@@ -79,6 +128,7 @@ function App() {
 
   async function sortear() {
     if (!isSpinning) {
+      setWinner(null)
       setIsSpinning(true);
       let canvas = document.getElementById("idcanvas");
       let randomTime = Math.floor(Math.random() * 3000) + 3000; // Tiempo de giro aleatorio entre 3 y 6 segundos
@@ -88,7 +138,6 @@ function App() {
       let rotationAmount = 0;
       let maxRotationAmount = 10; // Ajusta este valor para cambiar la velocidad de rotación
   
-      setRotation(0); // Establece el ángulo inicial como 0
   
       while (rotationAmount < totalRotation) {
         rotationAmount += maxRotationAmount;
@@ -102,7 +151,7 @@ function App() {
   
       // Calcula el índice del ganador en base al ángulo final de la ruleta
       let winnerPosition = Math.floor((array_concursantes.length - ((finalAngle+91) % 360) / (360 / array_concursantes.length)) % array_concursantes.length);
-      setWinner(array_concursantes[winnerPosition]);
+      setWinner(array_concursantes[winnerPosition].nombre);
   
       setIsSpinning(false);
     }
@@ -111,7 +160,7 @@ function App() {
   return (
     <main>
       <div className="contenedor">
-        <h1>Concursantes</h1>
+        <h1 style={{height:"37px"}}>{winner&&winner}</h1>
         <div className="concursantes">
           <div className="canvas-container">
             <div className='centrado'></div>
@@ -125,8 +174,10 @@ function App() {
             
           </div>
         </div>
+        <div className='tablaConcursantes'>
+          <Table columns={columns} dataSource={array_concursantes} pagination={false}/>
+        </div>
       </div>
-      <div>{winner && <p>¡El ganador es: {winner}!</p>}</div>
     </main>
   );
 }
