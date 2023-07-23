d57133b9736d6bbef48d53b19d0de3a4b8d67860
jperezcoca <jperezcoca@ceit.es>
2023-07-23 12:32:17 +0200

    funcion de ruleta y ganador añadidas

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,128 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const array_concursantes = [
+    //'kamila Villablanca'-GANADORA
+    'Julen',
+    'Alex',
+    'Txipi',
+    'Iban',
+    'Iker',
+    'Avellano',
+    'Jongar',
+    'Sanchito',
+    'Marcos',
+  ];
+
+  const [rotation, setRotation] = useState(0);
+  const [isSpinning, setIsSpinning] = useState(false);
+  const [winner, setWinner] = useState(null);
+
+  useEffect(() => {
+    let canvas = document.getElementById("idcanvas");
+    let context = canvas.getContext("2d");
+    let center = canvas.width / 2;
+
+    context.beginPath();
+    context.moveTo(center, center);
+    context.arc(center, center, center, 0, 2 * Math.PI);
+    context.lineTo(center, center);
+    context.fillStyle = '#33333333';
+    context.fill();
+
+    context.beginPath();
+    context.moveTo(center, center);
+    context.arc(center, center, center - 10, 0, 2 * Math.PI);
+    context.lineTo(center, center);
+    context.fillStyle = 'black';
+    context.fill();
+
+    for (let i = 0; i < array_concursantes.length; i++) {
+      context.beginPath();
+      context.moveTo(center, center);
+      context.arc(center, center, center - 20, i * 2 * Math.PI / array_concursantes.length, (i + 1) * 2 * Math.PI / array_concursantes.length);
+      context.lineTo(center, center);
+      context.fillStyle = random_color();
+      context.fill();
+
+      context.save();
+      context.translate(center, center);
+      context.rotate(3 * 2 * Math.PI / (5 * array_concursantes.length) + i * 2 * Math.PI / array_concursantes.length);
+      context.translate(-center, -center);
+      context.font = "13px Comic Sans MS";
+      context.textAlign = "right";
+      context.fillStyle = "white";
+      context.fillText(array_concursantes[i], canvas.width - 30, center);
+      context.restore();
+    }
+  }, []);
+
+  function random_color() {
+    let ar_digit = ['2', '3', '4', '5', '6', '7', '8', '9'];
+    let color = '';
+    let i = 0;
+    while (i < 6) {
+      let pos = Math.round(Math.random() * (ar_digit.length - 1));
+      color = color + '' + ar_digit[pos];
+      i++;
+    }
+    return '#' + color;
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async function sortear() {
+    if (!isSpinning) {
+      setIsSpinning(true);
+      let canvas = document.getElementById("idcanvas");
+      let randomTime = Math.floor(Math.random() * 3000) + 3000; // Tiempo de giro aleatorio entre 3 y 6 segundos
+      let intervalTime = 10;
+      let totalRotation = 360 * (randomTime / 1000); // Calcula la rotación total en grados
+  
+      let rotationAmount = 0;
+      let maxRotationAmount = 10; // Ajusta este valor para cambiar la velocidad de rotación
+  
+      setRotation(0); // Establece el ángulo inicial como 0
+  
+      while (rotationAmount < totalRotation) {
+        rotationAmount += maxRotationAmount;
+        let newAngle = rotationAmount % 360;
+        canvas.style.transform = "rotate(" + newAngle + "deg)";
+        await sleep(intervalTime);
+      }
+  
+      // Calcula el ángulo final de la ruleta
+      let finalAngle = rotationAmount % 360; // Asegura que el ángulo final esté en el rango de 0 a 360 grados
+  
+      // Calcula el índice del ganador en base al ángulo final de la ruleta
+      let winnerPosition = Math.floor((array_concursantes.length - ((finalAngle) % 360) / (360 / array_concursantes.length)) % array_concursantes.length);
+      setWinner(array_concursantes[winnerPosition]);
+  
+      setIsSpinning(false);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="contenedor">
+        <h1>Concursantes</h1>
+        <div className="concursantes">
+          <canvas id="idcanvas" width="600" height="600"></canvas>
+          <br />
+          <button onClick={() => { sortear() }}>
+            <span id="idestado">{isSpinning ? "Detener" : "Sortear"}</span>
+          </button>
+          <div className="mark-winner">
+            
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div>{winner && <p>¡El ganador es: {winner}!</p>}</div>
+    </main>
+  );
 }
 
 export default App
