7e34a1bb13f66db79d233ee93cd9f8adb377be06
jperezcoca <jperezcoca@ceit.es>
2023-07-23 16:19:02 +0200

    añadida opcion para añadir +de 1 y eliminar + de 1

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { HexColorPicker } from "react-colorful";
 
 function App() {
 
+  const [nuevosParticipantes,setNuevosParticipantes] = useState(1)
+  const [deletePart,setDeletePart] = useState(1)
   const [array_concursantes, setArray_concursantes] = useState([
     {
       key:1,
@@ -19,9 +21,56 @@ function App() {
     }
   ]);
 
+
+  const addParticipante = () => {
+
+    let cambios = [...array_concursantes]
+
+    const nuevosParticipantesArray = Array.from({ length: nuevosParticipantes });
+
+
+    nuevosParticipantesArray.forEach((_,index) => {
+      const newP = {
+          key: new Date().getTime()+ index,
+          nombre: array_concursantes.length + index + 1,
+          color: random_color()
+      };
+      cambios.push(newP);
+  });
+
+
+    setArray_concursantes(cambios)
+  }
+
+  const deleteParticipantes = () => {
+    console.log(deletePart);
+    console.log(array_concursantes.length);
+    let cambios = [...array_concursantes]
+    if(array_concursantes.length<= 2) return
+    if(array_concursantes.length<=parseInt(deletePart)){
+      if(array_concursantes.length===parseInt(deletePart)){
+        let toDelete = array_concursantes.length -2
+        cambios.splice(-toDelete)
+      }else{
+        let toDelete = parseInt(deletePart)-array_concursantes.length-1
+        cambios.splice(-toDelete)
+      }
+    }else{
+      cambios.splice(-deletePart)
+    }
+    
+    setArray_concursantes(cambios)
+  }
+
+  const eliminarParticipante = (item:any) => {
+    if(array_concursantes.length >= 3 ){
+      setArray_concursantes(array_concursantes.filter(i => i.key != item.key))
+    }
+  }
+
   const changeColor = (color,item) => {
 
-    let cambios = [...arrayCambiosCon]
+    let cambios = [...array_concursantes]
     item.color = color
 
     setArray_concursantes(cambios.map((i)=>{
@@ -31,21 +80,35 @@ function App() {
     
   }
 
+  const changeNombre = (text,item) => {
+
+    let cambios = [...array_concursantes]
+    item.nombre = text
+
+    setArray_concursantes(cambios.map((i)=>{
+      if(item.key===i.key){return item}
+      return i
+    }))
+  }
+
   interface DataType {
     key:number;
-    nombre: string;
+    nombre: any;
     color: any;
   }
 
   const columns : ColumnsType<DataType> = [
     {
       title:"Nombre",
-      dataIndex:"nombre",
-      render: (text)=><p>{text}</p>
+      render: (item)=><input type="text" name="nombre" value={item.nombre} onChange={(t)=>{changeNombre(t.target.value,item)}}/>
     },
     {
       title:"Color",
       render: (item)=><section className="small"><HexColorPicker color={item.color} onChange={(color)=>{changeColor(color,item)}} /></section>
+    },
+    {
+      title:"Eliminar",
+      render:(item)=><button onClick={()=>{eliminarParticipante(item)}} disabled={array_concursantes.length <= 2}>Eliminar</button>
     }
   ]
 
@@ -65,6 +128,15 @@ function App() {
 
   const [isSpinning, setIsSpinning] = useState(false);
   const [winner, setWinner] = useState(null);
+  function hexToLuma(hexColor) {
+    // Obtener los componentes de color en formato decimal
+    const r = parseInt(hexColor.substr(1, 2), 16);
+    const g = parseInt(hexColor.substr(3, 2), 16);
+    const b = parseInt(hexColor.substr(5, 2), 16);
+
+    // Calcular la luma del color
+    return 0.2126 * r + 0.7152 * g + 0.0722 * b;
+}
 
   useEffect(() => {
     let canvas = document.getElementById("idcanvas");
@@ -75,7 +147,7 @@ function App() {
     context.moveTo(center, center);
     context.arc(center, center, center, 0, 2 * Math.PI);
     context.lineTo(center, center);
-    context.fillStyle = '#33333333';
+    context.fillStyle = 'black';
     context.fill();
 
     context.beginPath();
@@ -92,23 +164,43 @@ function App() {
       context.lineTo(center, center);
       context.fillStyle = array_concursantes[i].color;
       context.fill();
-
+      let backgroundLuma = hexToLuma(array_concursantes[i].color)
+      let textColor = backgroundLuma < 128 ? "white" : "black";
       context.save();
       context.translate(center, center);
       context.rotate(3 * 2 * Math.PI / (5 * array_concursantes.length) + i * 2 * Math.PI / array_concursantes.length);
       context.translate(-center, -center);
-      context.font = "27px Comic Sans MS";
+      function findFontSizeToFitText(text, maxWidth, fontName) {
+        let minSize = 1;
+        let maxSize = 75; // Valor arbitrario, puedes ajustarlo según tus necesidades
+
+        while (minSize < maxSize) {
+            const midSize = Math.ceil((minSize + maxSize) / 2);
+            context.font = `${midSize}px ${fontName}`;
+            const textWidth = context.measureText(text).width;
+
+            if (textWidth+2 <= maxWidth-30) {
+                minSize = midSize;
+            } else {
+                maxSize = midSize - 1;
+            }
+        }
+
+        return minSize;
+    }
+
+    // Calcular el tamaño del texto dinámicamente para que se ajuste dentro del botón (arco)
+    var text = array_concursantes[i].nombre;
+    var availableWidth = Math.abs(2 * Math.PI * (center - 20) / array_concursantes.length);
+
+    // Encontrar el tamaño de fuente adecuado utilizando búsqueda binaria
+    var fontSize = findFontSizeToFitText(text, availableWidth, "Comic Sans MS");
       context.textAlign = "right";
-      context.fillStyle = "white";
+      context.fillStyle = textColor;
       context.fillText(array_concursantes[i].nombre, canvas.width - 35, center);
       context.restore();
     }
   }, [array_concursantes]);
-
-  const cambiarConcursantes = () => {
-    setArray_concursantes(arrayCambiosCon)
-  }
-
 
   function random_color() {
     let ar_digit = ['2', '3', '4', '5', '6', '7', '8', '9'];
@@ -175,6 +267,10 @@ function App() {
           </div>
         </div>
         <div className='tablaConcursantes'>
+          <button onClick={()=>{addParticipante()}}>AGREGAR OPCIÓN</button>
+          <input type="number" name="numeroAañadir" min={1} value={nuevosParticipantes} onChange={(t)=>{setNuevosParticipantes(t.target.value)}}/>
+          <button onClick={()=>{deleteParticipantes()}} disabled={array_concursantes.length<= 2}>Eliminar del final</button>
+          <input type="number" name="numeroAañadir" min={1} max={array_concursantes.length-2} value={deletePart} onChange={(t)=>{setDeletePart(t.target.value)}} disabled={array_concursantes.length<= 2}/>
           <Table columns={columns} dataSource={array_concursantes} pagination={false}/>
         </div>
       </div>
